42a92e4b9523cf7eb76de2392411e910aa4b36ee
Maxime <metalmax312@hotmail.fr>
2025-05-17 16:31:50 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -18,11 +18,16 @@ window.addEventListener('DOMContentLoaded', function () {
         });
 
     function afficherMessage(sender, message, optionCliquee = null) {
+        // function poiur afficher un message dans le chat
         const msg = document.createElement('div');
         msg.className = sender === 'Maxime' ? 'message bot' : 'message user';
+        // ajout la classe messsage bot  si c'est un message du bot sinon messsage user
         msg.textContent = `${sender}: ${message}`;
+        // ajout le texte du message
         chatContainer.appendChild(msg);
+        // ajout le message au chat
         chatContainer.scrollTop = chatContainer.scrollHeight;
+        // faire défiler le chat vers le bas
 
         chatReponse.innerHTML = '';
         if (sender === 'Maxime') {
@@ -36,18 +41,31 @@ window.addEventListener('DOMContentLoaded', function () {
     function afficherOptions(options, optionsExclues = []) {
         chatReponse.innerHTML = '';
 
-        options
-            .filter(option => !optionsExclues.includes(option))
-            .forEach(option => {
-                const btn = document.createElement('button');
-                btn.textContent = option;
-                btn.addEventListener('click', () => {
-                    const oldButtons = chatContainer.querySelectorAll('button');
-                    oldButtons.forEach(b => b.remove());
-                    afficherMessage('Vous', option);
-                    afficherMessage('Maxime', data.options[option], option);
-                });
-                chatContainer.appendChild(btn);
+        const autresOptions = options.filter(option => option !== 'cv' && !optionsExclues.includes(option));
+        const cvOption = options.includes('cv') && !optionsExclues.includes('cv');
+
+        autresOptions.forEach(option => {
+            const btn = document.createElement('button');
+            btn.textContent = option;
+            btn.addEventListener('click', () => {
+                const oldButtons = chatContainer.querySelectorAll('button');
+                oldButtons.forEach(b => b.remove());
+                afficherMessage('Vous', option);
+                afficherMessage('Maxime', data.options[option], option);
             });
+            chatContainer.appendChild(btn);
+        });
+
+        if (autresOptions.length === 0 && cvOption) {
+            const btn = document.createElement('button');
+            btn.textContent = 'cv';
+            btn.addEventListener('click', () => {
+                const oldButtons = chatContainer.querySelectorAll('button');
+                oldButtons.forEach(b => b.remove());
+                afficherMessage('Vous', 'cv');
+                afficherMessage('Maxime', data.options['cv'], 'cv');
+            });
+            chatContainer.appendChild(btn);
+        }
     }
 });
